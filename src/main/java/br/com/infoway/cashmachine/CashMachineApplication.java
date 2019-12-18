04b9da1b95140f7c9e01762739cf68c11f89a263
Wildrimak <finalwildrimak@gmail.com>
2019-12-17 22:14:29 -0300

    rt-01 projeto inicial criado

---
+++ src/main/java/br/com/infoway/cashmachine/CashMachineApplication.java
@@ -0,0 +1,13 @@
+package br.com.infoway.cashmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/infoway/cashmachine/CashMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.infoway.cashmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
