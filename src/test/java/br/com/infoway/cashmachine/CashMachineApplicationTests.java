4e1bed74f546cf8a153ea29dab0342f5f319354a
Wildrimak <finalwildrimak@gmail.com>
2019-12-22 18:46:54 -0300

    retirando lançador de exception de contextLoads da classe principal de testes

--- src/test/java/br/com/infoway/cashmachine/CashMachineApplicationTests.java
+++ src/test/java/br/com/infoway/cashmachine/CashMachineApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CashMachineApplicationTests {
 
 	@Test
-	public void contextLoads() throws Exception {
+	public void contextLoads() {
 	}
 
 }
