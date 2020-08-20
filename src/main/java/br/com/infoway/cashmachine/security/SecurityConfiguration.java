78eb1d1550dea2c09addba59e06ab2a5a66d2ed7
Wildrimak <finalwildrimak@gmail.com>
2020-08-20 14:59:57 -0300

    Desabilitado oauth temporariamente

--- src/main/java/br/com/infoway/cashmachine/security/SecurityConfiguration.java
+++ src/main/java/br/com/infoway/cashmachine/security/SecurityConfiguration.java
@@ -1,30 +1,20 @@
 package br.com.infoway.cashmachine.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//@EnableWebSecurity
+//@EnableAuthorizationServer
+//@EnableResourceServer
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+////	@Bean
+////	@Override
+//	protected AuthenticationManager authenticationManager() throws Exception {
+//		return super.authenticationManager();
+//	}
+//
+////	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+////		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+////		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("nimda")).roles("ADMIN");
+//	}
 
-@EnableWebSecurity
-@EnableAuthorizationServer
-@EnableResourceServer
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-		auth.inMemoryAuthentication().withUser("admin").password(encoder.encode("nimda")).roles("ADMIN");
-	}
-
-}
+//}
